5f0c2af4eee0c3e9f74489f2642120d4f60c4546
frank038 <42263206+frank038@users.noreply.github.com>
2021-09-05 16:08:26 +0200

    Add files via upload

---
+++ cfg.py
@@ -0,0 +1,34 @@
+# the height of the bar (it depends also on the font size)
+bar_size=32
+# 1 left - 0 right
+menu_left=1
+# to use with a compositor enabled: 0 no - 1 yes
+with_compositor=0
+# theme style: "" to use the default theme
+theme_style=""
+# icon theme: "" to use the default theme
+icon_theme=""
+# in the form "#XXXXXX" or "rgb(X, X, X)" or "colour name"
+scroll_handle_col="#B5B5B5"
+# the calendar to read
+calendar_file=""
+# in the form "#XXXXXX" or "colour name"
+calendar_appointment_day_color="#00AA00"
+# in the form "#XXXXXX" or "colour name"
+appointment_border_color="gray"
+# font to be used in the label
+calendar_label_font=""
+# font size
+calendar_label_font_size=9
+# font weight: Thin 0 - ExtraLight 12 - Light 25 - Normal 50 - Medium 57 - DemiBold 63 - Bold 75 - ExtraBold 81 - Black 87
+calendar_label_font_weight=75
+# font italic: 0 no - 1 yes
+calendar_label_font_italic=0
+#
+appointment_char="✔"
+#
+shutdown_command="shutdown -P"
+#
+restart_command="shutdown -r"
+#
+logout_command=""
